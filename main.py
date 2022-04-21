398bfc2c8bb527b4c0159c3ee5db106d8cbf9d7e
karunaeerapu <karuna.eerapu5023@gmail.com>
2022-04-21 19:51:45 -0400

    This is the first comment

---
+++ main.py
@@ -0,0 +1,32 @@
+# importing the lib
+
+from flask import Flask , render_template, request
+import joblib
+app = Flask(__name__)
+model = joblib.load('model/diabetic_80_joblib.pkl')
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/data', methods=['post'])
+def data():
+    preg = request.form.get('preg')
+    plas = request.form.get('plas')
+    pres = request.form.get('pres')
+    skin = request.form.get('skin')
+    test = request.form.get('test')
+    mass = request.form.get('mass')
+    pedi = request.form.get('pedi')
+    age = request.form.get('age')
+    result = model.predict([[preg, plas, pres, skin, test, mass, pedi, age]])
+
+    if result[0]==1:
+        data = 'person is diabatic'
+    else:
+        data = 'person is not diabatic'
+
+    print(data)
+    #return 'data received'
+    return render_template("predict.html")
+app.run(debug= True) # should be always at the end
+# any changes made are automatically considered.
